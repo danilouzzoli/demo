66de613e46cff77dc3fb3b70a4c5100acab85290
Danilo Uzzoli <danilo.uzzoli@gmail.com>
2024-11-26 10:22:17 +0100

    salvataggio prigetto intero

---
+++ src/main/java/com/example/demo/controller/MessageController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MessageController {
+
+  @GetMapping("/message")
+  public String displayMessage(Model model) {
+
+    model.addAttribute("message", "Hello, World!");
+    return "messageView";
+  }
+
+  @GetMapping("/message-without-gzip-compression")
+  public String displayMessageV2(Model model) {
+
+    model.addAttribute("message", "Hello, World!");
+    return "messageView";
+  }
+}
